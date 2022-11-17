b763a79d8cfa257475f17b90400433545c43ecce
Mohit Vaswani <mohitvaswani010@gmail.com>
2022-11-17 14:03:31 +0530

    React slider completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactCardSlider from "./slider";
 
 function App() {
+  const slides = [
+    {
+      image: "https://mohitvaswani.netlify.app/images/jobsearch.png",
+      title: "First Website",
+      description: "This is a 1st description",
+      link: "1"
+    },
+    {
+      image: "https://mohitvaswani.netlify.app/images/Movie.png",
+      title: "Second Website",
+      description: "This is a 2nd description",
+      link: "2"
+    },
+    {
+      image: "https://mohitvaswani.netlify.app/images/text.png",
+      title: "Third Website",
+      description: "This is a 3rd description",
+      link: "3"
+    },
+    {
+      image: "https://mohitvaswani.netlify.app/images/grocery.png",
+      title: "Fourth Website",
+      description: "This is a 4th description",
+      link: "4"
+    },
+    {
+      image: "https://mohitvaswani.netlify.app/images/podcast.png",
+      title: "Fifth Website",
+      description: "This is a 5th description",
+      link: "5"
+    },
+    {
+      image: "https://mohitvaswani.netlify.app/images/1.png",
+      title: "Sixth Website",
+      description: "This is a 6th description",
+      link: "6"
+    },
+    {
+      image: "https://mohitvaswani.netlify.app/images/text.png",
+      title: "Seventh Website",
+      description: "This is a 7th description",
+      link: "7"
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="slider">
+      <h1>Here are some amazing projects that I have made.</h1>
+      <ReactCardSlider slides={slides} />
     </div>
   );
 }
---
+++ src/slider.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Slider.css";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
+const openInNewTab = (url) => {
+  window.open(url, "_blank", "noopener,noreferrer");
+};
+
+const slider = (props) => {
+  const slideLeft = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  return (
+    <div id="main-slider-container">
+      <MdChevronLeft
+        size={40}
+        className="slider-icon left"
+        onClick={slideLeft}
+      />
+      <div id="slider">
+        {props.slides.map((slide, index) => {
+          return (
+            <div
+              className="slider-card"
+              key={index}
+              onClick={() => openInNewTab(`${slide.link}`)}
+            >
+              <div
+                className="slider-card-image"
+                style={{
+                  backgroundImage: `url(${slide.image})`,
+                  backgroundSize: "cover"
+                }}
+              ></div>
+              <p className="slider-card-title">{slide.title}</p>
+              <p className="slider-card-description">{slide.description}</p>
+            </div>
+          );
+        })}
+      </div>
+      <MdChevronRight
+        size={40}
+        className="slider-icon right"
+        onClick={slideRight}
+      />
+    </div>
+  );
+};
+export default slider;
